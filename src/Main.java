e1298f497f08a6111a53015fae9babc7c6989a88
Cameron Wian <camcanbuild@gmail.com>
2022-11-03 15:29:43 -0400

    Setup stuff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
+
+
 public class Main {
+    int green;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+    }
+    static void testmethod(){
+        System.out.println("testing");
     }
 }
