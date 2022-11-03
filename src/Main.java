3445e870b60b59a7e00b51c48e34c1ba3f4bc071
Cameron Wian <camcanbuild@gmail.com>
2022-11-03 15:42:12 -0400

    Setup stuff

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ import java.util.random.RandomGenerator;
 
 
 public class Main {
-    int green;
 
+
+    int green;
+    int orange;
+    int yellow;
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
     }
     static void testmethod(){
